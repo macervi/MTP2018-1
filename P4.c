7aaab1e217ca57ada3822bda05c77787dba53009
macervi <38115432+macervi@users.noreply.github.com>
2018-05-08 10:39:45 -0300

    Create P4.c

---
+++ P4.c
@@ -0,0 +1,18 @@
+\\MARINA CERVI ANGSTMAM
+\\11811EEL035
+#include <stdio.h>
+int main()
+{
+	int conv=0, cum=0, i=0;
+	char numero[256];
+	scanf("%s", numero);
+	getchar();
+	for (i=0; numero[i]; i++)
+	{
+		if ((numero[i]-'0')>=0 && (numero[i]-'0')<=9)
+			cum=cum*10+(numero[i]-'0');
+		else;
+	}
+	printf("%i", cum);
+	return 0;
+}
