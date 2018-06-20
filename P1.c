ad3063bc492a332f70ccc48deed5ad9911d041f8
macervi <38115432+macervi@users.noreply.github.com>
2018-06-20 17:36:07 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,31 +1,25 @@
-//Marina Cervi Angstmam
-//Matrícula: 11811EEL035
+/*Marina Cervi Angstmam
+Matrícula: 11811EEL035 */
 
-#include <stdio.h>
+#include<stdio.h>
 
 int main(){
-	int estado=0, i;
-	char seq[256];
-	printf("\nEntre: ");
-	scanf("%s", &seq);
+	int estado=0, i=0;
+	char bits[256];	
+	scanf("%s", bits);	
+	while(bits[i] != '\0') {		
+	if(bits[i]=='0'&&estado==0||bits[i]=='1'&&estado==1)
+			estado=0;
+	else if(bits[i]=='1'&&estado==0||bits[i]=='0'&&estado==2)
+			estado=1;
+	else if(bits[i]=='0'&&estado==1||bits[i]=='1'&&estado==2)
+			estado=2;
+			i++;
+	}
 
-for(i=0; seq[i]!= '\0'; i++){
-	if(estado==0 && seq[i]== '0'){
-		estado=0;}
-	else if(estado==0 && seq[i]=='1'){
-		estado=1;}
-	else if(estado==1 && seq[i]=='0'){
-		estado=2;}
-	else if(estado==1 && seq[i]=='1'){
-		estado=0;}
-	else if(estado==2 && seq[i]=='0'){
-		estado=1;}
-	else if(estado==2 && seq[i]=='1'){
-		estado=2;}
-}
-if(estado==0){printf("\nO  multiplo de 3. ");
-}
-else{printf("\nO nao e multiplo.");
-}
-return 0;
+	if(estado==0)
+	printf("%s e multiplo de 3\n", bits);
+	else
+	printf("%s nao e\n", bits);
+		return 0;
 }
