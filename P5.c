3fef83623f1346926687ab025ad5193b02534159
macervi <38115432+macervi@users.noreply.github.com>
2018-07-11 12:55:33 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,54 +1,90 @@
 /*MARINA CERVI ANGSTMAM
 11811EEL035*/
-#include<stdio.h>
 
-int main(){
+#include <stdio.h>
 
-	char str[256], *p, aux;
-	int num[64], *pi, op, i;
-	
-	for(i=0; i<256; i++)
-		str[i]='\0';
-	
-	printf("Escolha uma opcao:\n1 - Codificar\n2 - Descodificar\n");
-	scanf("%d", &op); getchar();
-	
-	if(op==1){
-		for(i=0; i<255; i++)
-		{
-			str[i] = getchar(); 
-			if(str[i]==10)
-			{
-				str[i]='\0';
-				break;		
-			}
-		}
-				
-		for(i=0; str[i]!='\0'; i=i+4)
-		{
-			pi = (int *) &str[i];
-			if(str[i+4]!='\0')
-				printf("%d, ", *pi);
-			else
-				printf("%d ", *pi);		
-		}
-	}
-	else if(op==2)
-	{	
-		for(i=0; i<64; i++)
-			num[i]=0;
-					
-		for(i=0; i<64; i++)
-		{
-			scanf("%d%c", &num[i], &aux);//mostrar e ler numeros
-			p = (char *) num;
-			if(aux==10)
-				break;
-		}
-		
-		printf("%s\n", p);
-	}
-		
-	return 0;
-}
+int main ()
+
+{
+
+    int x,y,D[64];
+
+    char C [255];
+
+    int* pcod = (int*) C;
+
+    char* pdec = (char*) &D;
+    
+	printf("Bem Vindo(a), escolha a opcao desejada:\n");
+    printf("1-> Codificar\n");
+    printf("2-> Decodificar\n");
+
+    scanf("%d", &x);
+
+    y=getchar();
+
+    switch(x)
+
+    {
+
+    case 1:
+
+        for(y=0;y<255;y++)
+
+        
+
+            C[y]=0;
+
+        
+
+        printf("\n->Digite uma frase com o maximo de 255 caracteres\n");
+
+        fgets(C,255,stdin);
+
+        for(y=0;C[y]!=0;y++)
+
+            if(C[y]==10){C[y]=0;}
+
+        
+
+        for(y=0;*(pcod+y)!=0;y++)
+
+            printf("%d ", *(pcod+y));
+
+        
+
+        break;
+
+    case 2:
+
+        printf("\n-Digite um numero\n");
+
+        for(y=0;y<64;y++)
+
+        {
+
+            scanf("%d", &D[y]);
+
+            if(getchar()=='\n')
+
+            {
+
+                break;
+
+            }
+
+        }
+
+        for (y=0;*(pdec+y)!=0;y++)
+
+        printf("%c", *(pdec+y));
+
+        
+
+        break;
+
+    }
+
+    return 0;
+
 }
